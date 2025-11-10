603dbcb7c0bba393d0242b83f7b61739aad8e363
Shema Arsene <aniyonkurushema@gmail.com>
2025-11-10 16:41:59 +0200

    feat(backend): Add routes to the app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
+import authRoutes from "./routes/authRoutes.js"
+import productRoutes from "./routes/productRoutes.js"
+import orderRoutes from "./routes/orderRoutes.js"
 
 // Environment variables
 dotenv.config()
@@ -15,6 +18,11 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Routes
+app.use("/api/auth", authRoutes)
+app.use("/api/products", productRoutes)
+app.use("/api/orders", orderRoutes)
 
 // Middleware to catch errors
 app.use(
